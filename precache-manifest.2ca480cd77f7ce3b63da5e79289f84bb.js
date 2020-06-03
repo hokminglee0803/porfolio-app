de4c38eda38d733ad9dabefbba7bbb59809624f7
hokminglee0803 <59437566+hokminglee0803@users.noreply.github.com>
2020-06-03 18:30:03 +0800

    Updates

---
+++ precache-manifest.2ca480cd77f7ce3b63da5e79289f84bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24103e9e11a975080727a8289cf8c03",
+    "url": "/porfolio-app/index.html"
+  },
+  {
+    "revision": "1d3eafef8974931aa261",
+    "url": "/porfolio-app/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "51461904753546ed805a",
+    "url": "/porfolio-app/static/js/2.cac5fe10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/porfolio-app/static/js/2.cac5fe10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3eafef8974931aa261",
+    "url": "/porfolio-app/static/js/main.518f1d92.chunk.js"
+  },
+  {
+    "revision": "feac8f1ec9163c558fab",
+    "url": "/porfolio-app/static/js/runtime-main.71de3a3a.js"
+  }
+]);
